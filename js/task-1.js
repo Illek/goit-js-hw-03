c0c907cc1e6c7f396498b76527406b6b3a2fd8de
Illek <dvrheim@gmail.com>
2024-04-01 02:40:13 +0300

    JS HW - 03

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,9 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
-  if (totalPrice <= customerCredits) {
-    return `Ви замовили ${quantity} дроїдів на суму ${totalPrice} кредитів!`;
-  } else {
-    return "Недостатньо коштів!";
-  }
+function slugify(title) {
+  const toSlug = title.toLowerCase().split(" ").join("-");
+  return toSlug;
 }
 
-// Тестові випадки
-console.log(makeTransaction(5, 3000, 23000)); // "Ви замовили 5 дроїдів на суму 15000 кредитів!"
-console.log(makeTransaction(3, 1000, 15000)); // "Ви замовили 3 дроїди на суму 3000 кредитів!"
-console.log(makeTransaction(10, 5000, 8000)); // "Недостатньо коштів!"
-console.log(makeTransaction(8, 2000, 10000)); // "Недостатньо коштів!"
-console.log(makeTransaction(10, 500, 5000)); // "Ви замовили 10 дроїдів на суму 5000 кредитів!"
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,11 @@
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  } else {
-    return message.slice(0, maxLength) + "...";
-  }
+function makeArray(firstArray, secondArray, maxLength) {
+  const stackArrays = firstArray.concat(secondArray).slice(0, maxLength);
+  return stackArrays;
 }
 
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-// ? Я зрозумів, що треба порівнювати maxLength саме з довжиною рядка "message",
-// ? але мені не зрозуміло, чому, в такому разі, якщо цього не зробити, воно буде додавати "..." всюди,
-// ? якщо ця дія оголошена тільки в else і не має виконуватись там, де довжина прийнятна?
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
